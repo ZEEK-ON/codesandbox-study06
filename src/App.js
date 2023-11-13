ac5c42e2da62ecdef96427c6f3ab4b471d91c0fd
イワシの匠 <44769766+ZEEK-ON@users.noreply.github.com>
2023-11-13 07:57:57 +0000

    Update 11 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { CssModules } from "./components/cssModules";
+import { Emotion } from "./components/emotion";
+import { InlineStyle } from "./components/inlineStyle";
+import { StyledComponents } from "./components/styledComponents";
+import { StyledJsx } from "./components/styledJsx";
 import "./styles.css";
+// import Emotion from "emotion/dist/emotion.cjs";
+// import { css } from "./components/@emotion/css";
+// import { Emotion } from "./components/Emotion";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
